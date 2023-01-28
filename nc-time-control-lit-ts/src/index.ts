6b535bf7f868f210fe3271001fb367d91e7d07b6
Andres Giraldo Arenas <andresganc@gmail.com>
2023-01-28 11:11:27 -0500

    New: NC Time Control Lit TS & NC Time Control React JS

---
+++ nc-time-control-lit-ts/src/index.ts
@@ -0,0 +1,1 @@
+export { NcTimeControlLitTs } from './NcTimeControlLitTs.js';
---
+++ nc-time-control-lit-ts/src/nc-time-control-lit-ts.ts
@@ -0,0 +1,3 @@
+import { NcTimeControlLitTs } from './NcTimeControlLitTs.js';
+
+window.customElements.define('nc-time-control-lit-ts', NcTimeControlLitTs);
